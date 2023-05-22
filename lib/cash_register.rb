075be723d427b3bda75f000e5f163c00401829fd
Charles Onguti <charles.onguti@student.moringaschool.com>
2023-05-22 14:43:15 +0300

    update

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -8,7 +8,7 @@ class CashRegister
     end
   
     def add_item(title, price, quantity = 1)
-      quantity.times { @items << { title: title, price: price } }
+      quantity.times { @items << { title: title, price: price, quantity: quantity } }
       @total += price * quantity
     end
   
@@ -28,8 +28,19 @@ class CashRegister
   
     def void_last_transaction
       last_item = @items.pop
-      @total -= last_item[:price]
+      @total -= last_item[:price] * last_item[:quantity]
       @total = 0.0 if @items.empty?
     end
   end
   
+
+  expect(cash_register_with_discount.employee_discount).to eq(20)
+  # Initialize with a 10% employee discount
+  expect(cash_register_with_discount.apply_discount).to eq("After the discount, the total comes to $800.0.")
+
+register.add_item("Item 1", 10) # Add an item with price $10
+register.apply_discount         # Apply the discount
+puts register.total             # Print the current total
+puts register.items             # Print the items
+register.void_last_transaction  # Void the last transaction
+puts register.total             # Print the current total after voiding
