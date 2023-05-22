5d42e4316c88ce2363f0ed0468ad8da6ec589721
Charles Onguti <charles.onguti@student.moringaschool.com>
2023-05-22 14:29:27 +0300

    update

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,47 +1,35 @@
 class CashRegister
+    attr_accessor :total, :employee_discount
+  
     def initialize(employee_discount = 0)
       @total = 0
       @employee_discount = employee_discount
       @items = []
     end
   
-    def total
-      @total
-    end
-  
     def add_item(title, price, quantity = 1)
+      quantity.times { @items << { title: title, price: price } }
       @total += price * quantity
-      quantity.times { @items << title }
     end
   
     def apply_discount
       if @employee_discount > 0
-        discount_amount = (@total * @employee_discount / 100.0).round(2)
+        discount_amount = @total * @employee_discount / 100.0
         @total -= discount_amount
-        "Success! Your discounted total is $#{@total}."
+        "After the discount, the total comes to $#{@total}."
       else
-        "Sorry, there is no discount to apply."
+        "There is no discount to apply."
       end
     end
   
     def items
-      @items
+      @items.map { |item| item[:title] }
     end
   
     def void_last_transaction
       last_item = @items.pop
-      item_price = item_price(last_item)
-      @total -= item_price
+      @total -= last_item[:price]
       @total = 0.0 if @items.empty?
-    end
-  
-    private
-  
-    def item_price(item)
-      # Retrieve the price of the item from your data source
-      # You can implement this based on your specific requirements
-      # This is just a placeholder implementation
-      10.0
     end
   end
   
